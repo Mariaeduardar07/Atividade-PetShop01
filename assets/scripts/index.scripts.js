a5dfe93b8a43c1f8b4dff5e1f3db9482a31c8e6a
maria <mariaeduardareistds2.senai@gmail.com>
2025-02-18 11:54:52 -0300

    feat: create function enviarEmail

--- assets/scripts/index.scripts.js
+++ assets/scripts/index.scripts.js
@@ -1,4 +1,38 @@
-function test() {
-    const nome = document.getElementById("nome").value;
-    alert(nome);
+// 
+
+function enviarEmail() {
+  const public_key = "El7W1iDkpIByI4RwB";
+  const secret_key = "9_MBsXwTAQcNHLk456_Yt";
+
+  (function () {
+    emailjs.init(public_key);
+  })();
+      const nome = document.getElementById('nome').value;
+      const email = document.getElementById('email').value;
+      const mensagem = document.getElementById('mensagem').value;
+
+      if (!nome || !email || !mensagem) {
+        alert("Por favor, preencha todos os campos");
+        return;
+      }
+
+      const params = {
+        nome: nome,
+        email: email,
+        mensagem: mensagem
+      };
+
+      const serviceId = "service_0xcltct";
+      const templateId = "template_a7fdelp";
+
+      emailjs
+        .send(serviceId, templateId, params)
+        .then((res) => {
+          document.getElementById("nome").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("mensagem").value = "";
+          console.log(res);
+          alert("Sua mensagem foi enviado para o Cantinho Pet com sucesso!");
+        })
+        .catch((err) => console.log(err));
 }
